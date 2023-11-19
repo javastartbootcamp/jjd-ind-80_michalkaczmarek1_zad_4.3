b17b64e42f3c54b061725905d16733d9b9a00ad3
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-11-19 22:32:45 +0100

    create shape calculator

---
+++ src/main/java/pl/javastart/task/data/Circle.java
@@ -0,0 +1,17 @@
+package pl.javastart.task.data;
+
+public class Circle {
+    private double radius;
+
+    public Circle(final double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/main/java/pl/javastart/task/data/Square.java
@@ -0,0 +1,17 @@
+package pl.javastart.task.data;
+
+public class Square {
+    private double a;
+
+    public Square(final double a) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+}
